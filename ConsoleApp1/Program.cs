2b4d673ebba80448fb014e8a42f89f8bb64138ec
Hitesh Kumar <hiteshjnv12358@gmail.com>
2024-12-09 02:02:58 +0530

    jj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string s = "ram";
-            string s2 = "ram";
             string s3 = "ram";
             string s4 = "ram";
             string s5 = "ram";
